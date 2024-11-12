776446a1ca07315604b6d9eec8ba6aeffc9dcc91
Ron Gabriel Ramos <rongabriel01.ramos@gmail.com>
2024-11-12 21:19:33 +0800

    Add files via upload

---
+++ M01/navbar.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Yapper</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="icon" href="imgs/yapperlogo.png">
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+</head>
+
+<body>
+  <!-- NavBar -->
+  <nav class="navbar navbar-light navbar-expand-lg navBarContainer" style="background-color: aquamarine;">
+    <div class="container-fluid p-3">
+        <img class="img-fluid navBarLogo p-3" src="imgs/yapperlogohome.png">
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav ms-auto">
+                <a class="nav-item nav-link px-5" href="index.html">HOME</a>
+                <a class="nav-item nav-link px-5" href="messages.php">MESSAGES</a>
+                <a class="nav-item nav-link px-5" href="friends.php">FRIENDS</a>
+                
+                <div class="container-fluid px-5">
+                  <img class="img-fluid navBarLogo" src="imgs/zhonglipfp.png">
+                </div>
+            </div>
+        </div>
+       
+    </div>
+</nav>
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  
+</body>
+
+</html>
